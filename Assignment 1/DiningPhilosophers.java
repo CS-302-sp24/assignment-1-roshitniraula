413cd468de257f15a58d7d2fc81c6216f3e90a2d
Roshit Niraula <roshitniraula13@gmail.com>
2024-02-11 21:33:32 -0600

    updated code to print the required output

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,8 +1,17 @@
 public class DiningPhilosophers {
 
   public static void main(String[] args) throws InterruptedException {
-    Philosopher[] philosophers = new Philosopher[5];
-    Chopstick[] chopsticks = new Chopstick[5];
+   
+    // arguments parsed through command line and its five arguments 
+   int np = Integer.parseInt(args[0]); //noOfPhilosophers
+   int nc = Integer.parseInt(args[1]); //noOfCycles
+   int tt = Integer.parseInt(args(2)); //thinkingTime
+   int et = Integer.parseInt(args[3]); //eatingTime
+   int rl = Integer.parseInt(args[4]); //rightLeftIndex
+   //int nf = np; //noOfForks = noOfPhilosophers
+
+    Philosopher[] philosophers = new Philosopher[np];
+    Chopstick[] chopsticks = new Chopstick[np];
     
     for (int i = 0; i < 5; ++i)
       chopsticks[i] = new Chopstick(i);
--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -4,25 +4,145 @@ class Philosopher extends Thread {
   private Chopstick left, right;
   private Random random;
   private int thinkCount;
+  public int numCycles; // number of cycles ; condition if 0, number of cycles goes forever
+  public int thinkingTime; //maximum thinking time in milliseconds 
+  public int eatingTime; //maximum eating time  in milliseconds
+  public int philsopherID; //philosopherID unique to philosopher
+  public int rightHandIndex; // condition if 0, all philosophers right handed + try grabbing right chopstick first ;; //condition if 1, even right handed + odd left handed 
 
-  public Philosopher(Chopstick left, Chopstick right) {
-    this.left = left; this.right = right;
+
+  public Philosopher(Chopstick left, Chopstick right, int numCycles, int thinkingTime, int eatingTime, int philosopherID, int rightHandIndex) {
+    this.left = left; 
+    this.right = right;
     random = new Random();
+    
+    this.rightHanded = rightHanded; 
+    this.numCycles = numCycles; 
+    this.thinkingTime = thinkingTime; 
+    this.eatingTime = eatingTime; 
+    this.philsopherID = philosopherID; 
+    this.rightHandIndex = rightHandIndex;
+
+    if (numCycles == 0){
+      thinkCount = Integer.MAX_VALUE; // condition for 0 cycle goes forever
+    }
   }
 
   public void run() {
     try {
-      while(true) {
-        ++thinkCount;
-        if (thinkCount % 10 == 0)
-          System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
+      //Case A: rightHandIndex == 0, 
+      //all philosophers are right handed 
+      //try grabbing right chopsticks first
+
+      if (rightHandIndex == 0){
+        while (thinkCount < cycles){
+          ++thinkCount;
+        }
+        if (thinkCount % 10 == 0){
+          System.out.println("Philosopher " + philosopherID + " has thought " + thinkCount + " times");
+          int tt = nextInt(thinkTime);
+          System.out.println("Philosopher " + philosopherID + " thinks for " + tt + " units");
+          Thread.sleep(tt); 
+          
+          // The philosopher thinks for the given designated time. 
+          
+          System.out.println("Philosopher" + philosopherID + "wants right chopstick.")
+          
+          //The philosopher, since right handed first goes for right hand.
+          synchronized(right){
+            System.out.println("Philosopher" + philosopherID + "has right chopstick.")
+            System.out.println("Philosopher" + philosopherID + "wants left chopstick.")
+            synchronized(left){
+              System.out.println("Philosopher" + philosopherID + "has left chopstick.")
+              int et = nextInt(eatingTime);
+              System.out.println("Philosopher" + philosopherID + "eats for" + et + "units." )
+              
+              //Once the philosopher has both the chopsticks, they are ready to start eating 
+              Thread.sleep(et);
+            }
+            System.out.println("Philosopher" + philosopherID + "releases left chopstick.")
+          }
+            System.out.println("Philosopher" + philosopherID + "releases right chopstick.")
           }
         }
+
+
+      // Case B: rightHandIndex == 1,
+      // even number of philosophers are right handed 
+      // odd number of philosophers are left handed  
+
+      else{
+        // Case B1: provided the philosopher is right handed, 
+        // same as before 
+
+        if (philosopherID % 2 == 0 ){
+          while (thinkCount < cycles){
+            ++thinkCount;
+          }
+          if (thinkCount % 10 == 0){
+            System.out.println("Philosopher " + philosopherID + " has thought " + thinkCount + " times");
+            int tt = nextInt(thinkTime);
+            System.out.println("Philosopher " + philosopherID + " thinks for " + tt + " units");
+            Thread.sleep(tt); 
+            
+            // The philosopher thinks for the given designated time. 
+            
+            System.out.println("Philosopher" + philosopherID + "wants right chopstick.")
+            
+            //The philosopher, since right handed first goes for right hand.
+            synchronized(right){
+              System.out.println("Philosopher" + philosopherID + "has right chopstick.")
+              System.out.println("Philosopher" + philosopherID + "wants left chopstick.")
+              synchronized(left){
+                System.out.println("Philosopher" + philosopherID + "has left chopstick.")
+                int et = nextInt(eatingTime);
+                System.out.println("Philosopher" + philosopherID + "eats for" + et + "units." )
+                
+                //Once the philosopher has both the chopsticks, they are ready to start eating 
+                Thread.sleep(et);
+              }
+              System.out.println("Philosopher" + philosopherID + "releases left chopstick.")
+            }
+              System.out.println("Philosopher" + philosopherID + "releases right chopstick.")
+            }
+        }
+
+        // Case B2: provided the philosopher is left handed, 
+        //grabs left chopstick first and then right chopstick
+
+        else{
+          while (thinkCount < cycles){
+            ++thinkCount;
+          }
+          if (thinkCount % 10 == 0){
+            System.out.println("Philosopher " + philosopherID + " has thought " + thinkCount + " times");
+            int tt = nextInt(thinkTime);
+            System.out.println("Philosopher " + philosopherID + " thinks for " + tt + " units");
+            Thread.sleep(tt); 
+            
+            // The philosopher thinks for the given designated time. 
+            
+            System.out.println("Philosopher" + philosopherID + "wants left chopstick.")
+            
+            //The philosopher, since left handed now, they first go for left hand chopstick.
+            synchronized(left){
+              System.out.println("Philosopher" + philosopherID + "has left chopstick.")
+              System.out.println("Philosopher" + philosopherID + "wants right chopstick.")
+              synchronized(right){
+                System.out.println("Philosopher" + philosopherID + "has right chopstick.")
+                int et = nextInt(eatingTime);
+                System.out.println("Philosopher" + philosopherID + "eats for" + et + "units." )
+                
+                //Once the philosopher has both the chopsticks, they are ready to start eating 
+                Thread.sleep(et);
+              }
+              System.out.println("Philosopher" + philosopherID + "releases right chopstick.")
+            }
+              System.out.println("Philosopher" + philosopherID + "releases left chopstick.")
+            }
+        }
+
+      }
       }
     } catch(InterruptedException e) {}
   }
-}
